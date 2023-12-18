6816c2957646b26925163189a9ae19f50742da1a
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-12-18 17:00:15 +0100

    creating bottom tabs navigator

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -5,9 +5,72 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import HomeScreen from "../screens/HomeScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+
+  const BottomTabs = () => {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "Home",
+            tabBarLabelStyle: {
+              color: "#2980B9",
+            },
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="home" size={24} color="#2980B9" />
+              ) : (
+                <Ionicons name="home-outline" size={24} color="black" />
+              ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Profile"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "Profile",
+            tabBarLabelStyle: {
+              color: "#2980B9",
+            },
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="ios-person" size={24} color="#2980B9" />
+              ) : (
+                <Ionicons name="ios-person-outline" size={24} color="black" />
+              ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Cart"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "Cart",
+            tabBarLabelStyle: {
+              color: "#2980B9",
+            },
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="cart" size={24} color="#2980B9" />
+              ) : (
+                <Ionicons name="cart-outline" size={24} color="black" />
+              ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  };
 
   return (
     <NavigationContainer>
@@ -23,8 +86,8 @@ const StackNavigator = () => {
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name="Home"
-          component={HomeScreen}
+          name="Main"
+          component={BottomTabs}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
