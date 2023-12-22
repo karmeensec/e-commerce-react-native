d7b7414445c20f132f5b59b95c2fc81492d993d5
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-12-22 15:43:51 +0100

    creating an add to cart reducer

---
+++ redux/CartReducer.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const CartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    cart: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      const isItemPresent = state.cart.find(
+        (item) => item.id === action.payload.id
+      );
+
+      if (isItemPresent) {
+        isItemPresent.quantity++;
+      } else {
+        state.cart.push({ ...action.payload, quantity: 1 });
+      }
+    },
+
+    removeFromCart: (state, action) => {},
+  },
+});
