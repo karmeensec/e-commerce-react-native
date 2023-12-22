a75074de0641f6d47d0c84bddb79991223636c30
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-12-22 15:14:13 +0100

    Product information screen finished

--- screens/ProductInfoScreen.js
+++ screens/ProductInfoScreen.js
@@ -124,7 +124,7 @@ const ProductInfoScreen = () => {
                 justifyContent: "center",
                 flexDirection: "row",
                 backgroundColor: "#212F3D",
-                elevation: 5,
+                elevation: 20,
                 marginTop: "auto",
                 marginLeft: 20,
                 marginBottom: 20,
@@ -135,6 +135,117 @@ const ProductInfoScreen = () => {
           </ImageBackground>
         ))}
       </ScrollView>
+
+      <View style={{ padding: 10 }}>
+        <Text style={{ marginTop: 10, fontSize: 15, fontWeight: "500" }}>
+          {route?.params?.title}
+        </Text>
+        <Text style={{ marginTop: 6, fontSize: 18, fontWeight: "600" }}>
+          {" "}
+          ${route?.params?.price}{" "}
+        </Text>
+      </View>
+
+      <Text
+        style={{
+          height: 1,
+          borderColor: "#D7DBDD",
+          borderWidth: 1,
+          margin: 10,
+        }}
+      />
+
+      <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
+        <Text>Color: </Text>
+        <Text style={{ fontSize: 15, fontWeight: "bold" }}>
+          {route?.params?.color}
+        </Text>
+      </View>
+
+      <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
+        <Text>Size: </Text>
+        <Text style={{ fontSize: 15, fontWeight: "bold" }}>
+          {route?.params?.size}
+        </Text>
+      </View>
+
+      <Text
+        style={{
+          height: 1,
+          borderColor: "#D7DBDD",
+          borderWidth: 1,
+          margin: 10,
+        }}
+      />
+
+      <View style={{ padding: 10 }}>
+        <Text style={{ fontSize: 15, fontWeight: "bold", marginVertical: 5 }}>
+          Price: ${route?.params?.price}
+        </Text>
+        <Text style={{ color: "#239B56", fontWeight: "bold" }}>
+          Free delivery tomorrow by 3PM. Order till 2PM.
+        </Text>
+
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginVertical: 10,
+            gap: 5,
+          }}
+        >
+          <Ionicons name="location-outline" size={24} color="black" />
+          <Text style={{ fontSize: 15, fontWeight: "bold" }}>
+            Deliver to Kamil - Patrice Lumumby 16/18
+          </Text>
+        </View>
+      </View>
+
+      <Text
+        style={{
+          fontWeight: "bold",
+          marginHorizontal: 10,
+          color: "#F39C12",
+          fontSize: 20,
+          textAlign: "right",
+        }}
+      >
+        In Stock
+      </Text>
+
+      <Pressable
+        style={{
+          padding: 10,
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 10,
+          marginHorizontal: 10,
+          marginVertical: 10,
+          backgroundColor: "#F1C40F",
+          borderRadius: 20,
+          borderWidth: 1,
+          elevation: 10,
+        }}
+      >
+        <Text style={{ fontSize: 15, fontWeight: "bold" }}>Add to Cart</Text>
+      </Pressable>
+
+      <Pressable
+        style={{
+          padding: 10,
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 10,
+          marginHorizontal: 10,
+          marginVertical: 10,
+          backgroundColor: "#ECF0F1",
+          borderRadius: 20,
+          borderWidth: 1,
+          elevation: 10,
+        }}
+      >
+        <Text style={{ fontSize: 15 }}>Buy Now</Text>
+      </Pressable>
     </ScrollView>
   );
 };
