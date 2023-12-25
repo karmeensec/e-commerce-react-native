3571c9c7cb86a7b637b275979ecaecd23352e295
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-12-25 15:40:13 +0100

    create an endpoint to thet all the addresses

--- backend/index.js
+++ backend/index.js
@@ -194,3 +194,23 @@ app.post("/address", async (req, res) => {
     res.status(500).json({ message: "Error adding a new address" });
   }
 });
+
+// Get all the addresses of the specified user
+
+app.get("/address:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const addresses = user.address;
+    res.status(200).json({ addresses });
+  } catch (error) {
+    console.log("Getting all the addresses endpoint error: ", error);
+
+    res.status(500).json({ message: "Error getting addresses" });
+  }
+});
