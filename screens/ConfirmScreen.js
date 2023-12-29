24f1b8674a77578413462baa0525f023bd044b9d
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-12-29 19:59:43 +0100

    creating initial confirmation payment steps

---
+++ screens/ConfirmScreen.js
@@ -0,0 +1,80 @@
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+
+const ConfirmScreen = () => {
+  const steps = [
+    { title: "Address", content: "Address Form" },
+    { title: "Delivery", content: "Delivery Options" },
+    { title: "Payment", content: "Payment Details" },
+    { title: "Place Order", content: "Order Summary" },
+  ];
+
+  const [currentStep, setCurrentStep] = useState(0);
+
+  return (
+    <ScrollView style={{ marginTop: 45 }}>
+      <View style={{ flex: 1, paddingHorizontal: 20, paddingTop: 40 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            marginBottom: 20,
+          }}
+        >
+          {steps?.map((item, index) => (
+            <View
+              key={index}
+              style={{ justifyContent: "center", alignItems: "center" }}
+            >
+              {index > 0 && (
+                <View
+                  style={[
+                    { flex: 1, height: 2, backgroundColor: "green" },
+                    index <= currentStep && { backgroundColor: "green" },
+                  ]}
+                />
+              )}
+
+              <View
+                style={[
+                  {
+                    width: 30,
+                    height: 30,
+                    borderRadius: 15,
+                    backgroundColor: "#ccc",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  },
+                  index < currentStep && { backgroundColor: "green" },
+                ]}
+              >
+                {index < currentStep ? (
+                  <Text
+                    style={{ fontSize: 16, fontWeight: "bold", color: "white" }}
+                  >
+                    &#10003;
+                  </Text>
+                ) : (
+                  <Text
+                    style={{ fontSize: 16, fontWeight: "bold", color: "white" }}
+                  >
+                    {index + 1}
+                  </Text>
+                )}
+              </View>
+
+              <Text style={{ textAlign: "center", marginTop: 10 }}>
+                {item.title}
+              </Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default ConfirmScreen;
+
+const styles = StyleSheet.create({});
