8183ba1648aacc550c6e00ada3fad352acbb4f08
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-01-10 19:15:34 +0100

    initial animation for the order screen

---
+++ screens/OrderScreen.js
@@ -0,0 +1,52 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import LottieView from "lottie-react-native";
+
+const OrderScreen = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <LottieView
+        source={require("../assets/thumbs.json")}
+        // ref={animation}
+        style={{
+          height: 260,
+          width: 300,
+          alignSelf: "center",
+          marginTop: 40,
+          justifyContent: "center",
+        }}
+        autoPlay
+        loop={false}
+        speed={0.7}
+      />
+      <Text
+        style={{
+          marginTop: 20,
+          fontSize: 19,
+          fontWeight: "600",
+          textAlign: "center",
+        }}
+      >
+        Order has been received
+      </Text>
+      <LottieView
+        source={require("../assets/sparkle.json")}
+        style={{
+          height: 300,
+          position: "absolute",
+          top: 100,
+          width: 300,
+          alignSelf: "center",
+        }}
+        autoPlay
+        loop={false}
+        speed={0.7}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default OrderScreen;
+
+const styles = StyleSheet.create({});
