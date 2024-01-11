7f934ad9d77c852974b219dd11731f9abd6ac353
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-01-11 21:39:32 +0100

    bug fix related to path - schema

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-  name: {
+  user: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: "User",
